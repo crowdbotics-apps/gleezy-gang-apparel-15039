88231424e2ae8b0bcf8fd4409d391cfe0352851f
Crowdbotics <team@crowdbotics.com>
2020-03-24 23:20:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gleezy_gang_apparel_15039/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gleezy_gang_apparel_15039;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "gleezy_gang_apparel_15039";
+    }
+}
